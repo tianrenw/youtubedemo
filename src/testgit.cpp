c69f41be5818a553fe6154bd733c66849da13fcd
tianren <twang@robotex.com>
2013-09-17 16:45:12 -0700

    initial commit

---
+++ src/testgit.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : testgit.cpp
+// Author      : t
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
